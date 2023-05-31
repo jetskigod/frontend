befcd5eeab4c11e60ae521cc9484d136ba7332c8
jetskigod <jetskiyoungsins@gmail.com>
2023-05-31 11:38:47 +0700

    newjett3

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css';
+import Image from 'next/image';
 
 export default function index() {
   return (
-    <div>index</div>
+    <>
+    <Head>
+      <title>jett</title>
+    </Head>
+    
+<div className="text-center">
+  <Image src="/jet1.png" className="rounded" alt="jet1" width={500} height={250}/>
+</div>
+
+
+<div className="text-center"><button type="button" className="btn btn-dark">Dark</button></div>
+    </>
   )
 }
